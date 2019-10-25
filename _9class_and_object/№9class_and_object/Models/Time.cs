afb3753ae38a143cc366239cf16917198f9ff1be
101 <101@ctt.perm.ru>
2019-10-25 20:05:22 +0500

    onece fersion

---
+++ _9class_and_object/№9class_and_object/Models/Time.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9class_and_object
+{
+    public class Time : Attribute
+    {
+        public int hourse { get; set; }
+        public int minuts { get; set; }
+        [Time(hourse = 2, minuts = 23)]
+        class vipol
+        {
+
+        }
+        Time()
+        {
+            
+        }
+
+    }
+    
+}
---
+++ _9class_and_object/№9class_and_object/View/ITime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9class_and_object.View
+{
+    interface ITime
+    {
+        string houseText { set; get; }
+        string minutsText { set; get; }
+        string areaText { set; get; }
+    }
+}
